204a80a75e2fded4d01f86183d40efc930b76a97
Teodor Grozdanov <teodor.grozdanov@gmail.com>
2018-03-28 21:40:07 +0300

    Add BOT class
    Implementation of binary ordered tree specialised in counting inversions in a vector of numbers

---
+++ BOT.cpp
@@ -0,0 +1,152 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <climits>
+
+//Asserts there are predefined operators < and > for the type T
+
+template<class T>
+class BOT
+{
+	struct Node
+	{
+		T value;
+		Node* left, *right;
+		int rightSubT;				//# of nodes in right subT. Added for counting inversions
+		Node(const T&, Node* l = nullptr, Node* r = nullptr);
+	};
+
+	Node* copy(Node* crr);
+	void del(Node* crr);
+	void add(T el, Node*& crr);		//Adds equal to the right for the purpose of counting inversions correctly
+
+	Node* root;
+
+public:
+	int inversions;					//Added for counting inversions
+
+	BOT(std::vector<T> v = {});
+	BOT(const BOT<T>& other);
+	const BOT<T>& operator =(const BOT<T>& other);
+	~BOT();
+
+	void add(T);
+
+	template<class TT>
+	friend std::ostream& operator<< (std::ostream& os, const BOT<TT>& bot);
+};
+
+template<class T>
+BOT<T>::BOT(std::vector<T> v = {}) : root(nullptr), inversions(0)
+{
+	int size = v.size();
+	for (int i = 0; i < size; i++)
+		add(v[i]);
+}
+
+template<class T>
+typename BOT<T>::Node* BOT<T>::copy(Node* crr)
+{
+	if (crr)
+		return new Node(crr->value, copy(crr->left), copy(crr->right));
+	return nullptr;
+}
+
+template<class T>
+BOT<T>::Node::Node(const T& el, Node* l = nullptr, Node* r = nullptr) : value(el), left(l), right(r), rightSubT(0) {}
+
+template<class T>
+BOT<T>::BOT(const BOT<T>& other)
+{
+	root = copy(other.root);
+}
+
+template<class T>
+const BOT<T>& BOT<T>::operator =(const BOT<T>& other)
+{
+	if (this != &other)
+	{
+		del(root);
+		root = copy(other.root);
+	}
+	return *this;
+}
+
+template<class T>
+void BOT<T>::del(Node* crr)
+{
+	if (crr)
+	{
+		del(crr->left);
+		del(crr->right);
+		delete crr;
+	}
+}
+
+template<class T> 
+BOT<T>::~BOT()
+{
+	del(root);
+}
+
+template<class T>
+void BOT<T>::add(T el, Node*& crr)
+{
+	if (crr)
+	{
+		if (el < crr->value)
+		{
+			inversions += crr->rightSubT + 1;
+			add(el, crr->left);
+		}
+		else
+		{
+			(crr->rightSubT)++;
+			add(el, crr->right);
+		}
+	}
+	else
+		crr = new Node(el);
+}
+
+template<class T>
+void BOT<T>::add(T el)
+{
+	add(el, root);
+}
+
+template<class T>
+std::ostream& operator <<(std::ostream& os, const BOT<T>& bot)
+{
+	typename BOT<T>::Node* crr, *noSucc;
+	noSucc = (typename BOT<T>::Node*)INT_MIN;
+	std::queue<typename BOT<T>::Node*> q;
+	q.push(bot.root);
+	q.push(nullptr);
+	while (q.size() > 1)
+	{
+		crr = q.front();
+		q.pop();
+		if (crr != nullptr)
+		{
+			if (crr != noSucc)				
+			{
+				os << crr->value << ' ';
+				if (crr->left)
+					q.push(crr->left);
+				else q.push(noSucc);
+				if (crr->right)
+					q.push(crr->right);
+				else q.push(noSucc);
+			}
+			else os << "N ";
+		}
+		else
+		{
+			q.push(nullptr);
+			os << '\n';
+		}
+	}
+	return os;
+}
